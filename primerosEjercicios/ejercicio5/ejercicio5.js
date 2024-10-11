5ad5ebb719affb0bad6f79bd57a12a8db867229a
Fernando E Peró <fernandoepero@hotmail.com>
2024-10-10 22:01:21 -0300

    Se realizó el ejercicio N°5.

---
+++ primerosEjercicios/ejercicio5/ejercicio5.js
@@ -0,0 +1,7 @@
+/*  5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+*/ 
+
+let num1 = parseInt(prompt("Escriba un número"));
+let num2 = parseInt(prompt("Escriba otro número"));
+document.write(`La suma de sus números es ${num1+num2}`);
